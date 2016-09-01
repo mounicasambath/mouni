439f79a636bfcc07f5aa9738a052230ef34ef2a5
mounicasambath <mounicasambath@gmail.com>
2016-09-01 18:02:01 +0530

    Add files via upload

---
+++ days.java
@@ -0,0 +1,23 @@
+package workingdays;
+
+import java.util.Scanner;
+
+public class days {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s=new Scanner(System.in);
+
+String str=s.nextLine();
+if(str.equalsIgnoreCase("sunday"))
+{
+	System.out.println("false");
+}
+
+else
+{
+	System.out.println("true");
+}
+	}
+
+}
